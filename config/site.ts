39577e96e79a445d665966facb777dc1f7acdd72
TBP <tbp@TBPMacbook.local>
2023-10-06 13:25:27 +0700

    update op image

--- config/site.ts
+++ config/site.ts
@@ -5,7 +5,7 @@ export const siteConfig = {
   description:
     "Khám phá Lagi. Đánh giá, xếp hạng và đề xuất để tận dụng tối đa trải nghiệm cộng đồng của bạn.",
   url: "https://laginow.com",
-  ogImage: "https://laginow.com/opengraph-image.webp",
+  ogImage: "https://laginow.com/opengraph.webp",
   // links: {
   //   twitter: "https://twitter.com/shadcn",
   //   github: "https://github.com/shadcn/taxonomy",
