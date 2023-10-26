5f56246b7b9c97b6fa8e9d378bc21d7e3967a978
TBP <tbp@TBPMacbook.local>
2023-10-26 11:23:39 +0700

    update

--- app/api/editor/upload-image2/route.ts
+++ app/api/editor/upload-image2/route.ts
@@ -1,16 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
 import ImageKit from 'imagekit'
 import { imagekit } from '@/lib/imagekit';
-const { Storage } = require("@google-cloud/storage");
+import { Storage } from '@google-cloud/storage';
 import lagiJson from './lagi-361510-276b6fc08e21.json'
 
 const uploadImage = (fileName: string, buffer: any) => new Promise((resolve, reject) => {
   let projectId = "lagi-361510"; // Get this from Google Cloud
-  let keyFilename = './lagi-361510-276b6fc08e21.json'; // Get this from Google Cloud -> Credentials -> Service Accounts
+
   const storage = new Storage({
     projectId,
-    keyFilename,
-  });
+    credentials: lagiJson
+
+  })
 
   const bucket = storage.bucket("laginow");
   // const { originalname, buffer } = file
