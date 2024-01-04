95e6c8c909d75a084edb91e65a83e8a3bec76ed8
TBP <tbp@TBPMacbook.local>
2024-01-04 16:08:43 +0700

    update

--- app/(auth)/webview/page.tsx
+++ app/(auth)/webview/page.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
 import { headers } from 'next/headers'
+
   
 export default function Page(){
     const headersList = headers()
     const userAgent = headersList.get('User-Agent')
 
-    return <div  className="container flex h-screen w-screen flex-col items-center justify-center">
+    return <div  className="container px-2 flex h-screen w-screen flex-col items-center justify-center">
         {userAgent}
+        <p>Check out <a href="https:/laginow.com/" target="_blank" rel="noopener noreferrer">freeCodeCamp</a>.</p>
     </div>
 }
