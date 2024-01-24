e00b930ce4d294b56cd182450a54cf45cdb4c7c6
TBP <tbp@TBPMacbook.local>
2024-01-24 08:58:47 +0700

    update sitemap

--- app/(baiviet)/p/sitemap.ts
+++ app/(baiviet)/p/sitemap.ts
@@ -2,10 +2,6 @@
 import { MetadataRoute } from 'next'
 import prisma from '@/lib/prisma'
 
-export async function generateSitemaps() {
-  // Fetch the total number of products and calculate the number of sitemaps needed
-  return [{ id: 0 }]
-}
  
 
 const BASE_URL = 'https://www.laginow.com'
@@ -17,17 +13,17 @@ export default async function sitemap({
 }): Promise<MetadataRoute.Sitemap> {
 
   // Google's limit is 50,000 URLs per sitemap
-  const posts = await prisma.post.findMany({
+  const posts =  await prisma.post.findMany({
     where: {
-        published: true,
-        slug: {
-            not: null
-        }
+      slug: {
+        not: null
+      }
     }
-})
+  })
+ 
   return posts.map((item) => ({
     url: `${BASE_URL}/p/${item?.slug}`,
-    lastModified: item.createdAt,
+    lastModified: item?.createdAt,
     changeFrequency: 'monthly',
     priority: 0.5
   }))
